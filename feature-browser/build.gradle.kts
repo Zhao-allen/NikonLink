3c29f218643564995228eb391ee644ceeece01dd
koi <62574848+Zhao-allen@users.noreply.github.com>
2026-09-14 15:37:25 +0800

    1 2

--- feature-browser/build.gradle.kts
+++ feature-browser/build.gradle.kts
@@ -24,6 +24,8 @@ dependencies {
 
     implementation("com.google.dagger:hilt-android:2.48.1")
     ksp("com.google.dagger:hilt-compiler:2.48.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+
 
     implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
